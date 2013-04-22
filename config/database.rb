47a5aa484c9335f98d708616a9e648128adfb98a
Nat Welch <nat@natwelch.com>
2013-04-21 21:02:25 -0700

    padrino configured.

--- config/database.rb
+++ config/database.rb
@@ -1,48 +1,17 @@
 ##
-# You can use other adapters like:
-#
-#   ActiveRecord::Base.configurations[:development] = {
-#     :adapter   => 'mysql2',
-#     :encoding  => 'utf8',
-#     :reconnect => true,
-#     :database  => 'your_database',
-#     :pool      => 5,
-#     :username  => 'root',
-#     :password  => '',
-#     :host      => 'localhost',
-#     :socket    => '/tmp/mysql.sock'
-#   }
-#
-ActiveRecord::Base.configurations[:development] = {
-  :adapter => 'sqlite3',
-  :database => Padrino.root('db', 'what_development.db')
-
-}
-
-ActiveRecord::Base.configurations[:production] = {
-  :adapter => 'sqlite3',
-  :database => Padrino.root('db', 'what_production.db')
-
-}
-
-ActiveRecord::Base.configurations[:test] = {
-  :adapter => 'sqlite3',
-  :database => Padrino.root('db', 'what_test.db')
-
+# Database config for relational db.
+init = Time.now
+connections = {
+  :development => "postgres://localhost/what",
+  :test => "postgres://postgres@localhost/what_test",
+  :production => ENV['DATABASE_URL']
 }
 
 # Setup our logger
 ActiveRecord::Base.logger = logger
 
-# Raise exception on mass assignment protection for Active Record models.
-ActiveRecord::Base.mass_assignment_sanitizer = :strict
-
-# Log the query plan for queries taking more than this (works
-# with SQLite, MySQL, and PostgreSQL).
-ActiveRecord::Base.auto_explain_threshold_in_seconds = 0.5
-
 # Include Active Record class name as root for JSON serialized output.
-ActiveRecord::Base.include_root_in_json = false
+ActiveRecord::Base.include_root_in_json = true
 
 # Store the full class name (including module namespace) in STI type column.
 ActiveRecord::Base.store_full_sti_class = true
@@ -50,9 +19,34 @@ ActiveRecord::Base.store_full_sti_class = true
 # Use ISO 8601 format for JSON serialized times and dates.
 ActiveSupport.use_standard_json_time_format = true
 
-# Don't escape HTML entities in JSON, leave that for the #json_escape helper
-# if you're including raw JSON in an HTML page.
+# Don't escape HTML entities in JSON, leave that for the #json_escape helper.
+# if you're including raw json in an HTML page.
 ActiveSupport.escape_html_entities_in_json = false
 
-# Now we can establish connection with our db.
-ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[Padrino.env])
+# Now we can estabilish connection with our db
+if connections[Padrino.env]
+  url = URI(connections[Padrino.env])
+  options = {
+    :adapter => url.scheme,
+    :host => url.host,
+    :port => url.port,
+    :database => url.path[1..-1],
+    :username => url.user,
+    :password => url.password
+  }
+
+  case url.scheme
+  when "sqlite"
+    options[:adapter] = "sqlite3"
+    options[:database] = url.host + url.path
+  when "postgres"
+    options[:adapter] = "postgresql"
+  end
+
+  # Log what we are connecting to.
+  logger.bench "DB", init, "#{options.inspect}", :devel, :green
+
+  ActiveRecord::Base.establish_connection(options)
+else
+  logger.push("No database configuration for #{Padrino.env.inspect}", :fatal)
+end
