16013ab280eac638245b5ee26e8f3c8086bf55ad
Nat Welch <nat@natwelch.com>
2013-04-21 23:32:22 -0700

    login.

--- app/controllers.rb
+++ app/controllers.rb
@@ -8,8 +8,12 @@ What::App.controllers  do
     @idea = Idea.get(:id)
     render :idea
   end
+
+  get :login do
+    redirect "/auth/gplus"
+  end
   
-  get :success, :map => "/auth/:provider/callback" do
+  get "/auth/gplus/callback" do
     auth_hash = request.env["omniauth.auth"]
     session[:user_email] = auth_hash['email']
     flash[:message] = "You are now signed in as #{session[:user_email]}."
